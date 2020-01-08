0f441af4a42907b810051b0fd05828a9ee4b98d5
Ashish <42469420+ashishgarg880@users.noreply.github.com>
2020-01-08 14:25:27 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+class ifelse{
+  public static void main(String ar[])
+  {
+    int a=10;
+    int b=20;
+
+    if(a>b) 
+    {
+      System.out.print("A is less than B");
+    }
+    else
+    {
+      System.out.print("B is Greater Than A");
+    }
+  }
+
+}
