9ce5f088add7338dc513f70872c9ca8e08e0e49e
Ashish <42469420+ashishgarg880@users.noreply.github.com>
2020-01-08 14:33:01 +0530

    Create lifelse.java

---
+++ lifelse.java
@@ -0,0 +1,22 @@
+class lifelse
+{
+  public static void main(String ar[])
+  {
+    int a=10;
+    int b=20;
+    int c=30;
+    
+    if(a>b)
+    {
+      System.out.print("A is bigger");
+    }
+    ifelse(b>c)
+    {
+    System.out.print("B is Bigger");
+    }
+    else
+    {
+    System.out.print("C is Bigger than A & B");
+    }
+  }
+}
