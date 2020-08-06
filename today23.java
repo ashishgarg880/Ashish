0c2de3418627a209d8d253679e44b7de73dcd69d
Ashish <42469420+ashishgarg880@users.noreply.github.com>
2020-08-06 23:39:21 +0530

    Update today23.java

--- today23.java
+++ today23.java
@@ -1,10 +1,10 @@
-package chefdoof;
+package pack;
 import java.util.*;
 import java.lang.*;
 import java.io.*; 
 
 /* Name of the class has to be "Main" only if the class is public. */
-class chefdoof1
+class pack1
 {
 	public static void main(String[] args) throws Exception 
 	{
